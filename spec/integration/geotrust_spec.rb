f6f2be9b981bea3a9e1b4e219fe37ce1fbbbda9d
rubiii <me@rubiii.com>
2026-02-28 15:03:20 +0100

    Fix API usage in Geotrust spec
    Replaced pending specs with tests for
    what we can test atm.

--- spec/integration/geotrust_spec.rb
+++ spec/integration/geotrust_spec.rb
@@ -4,13 +4,34 @@ describe 'Integration with Geotrust' do
 
   subject(:client) { Sekken.new fixture('wsdl/geotrust') }
 
-  it 'knows the operations' do
-    pending "this fixture is missing a message element! " \
-            "find out if we need to handle this case or if the fixture is incomplete."
+  it 'returns a map of services and ports' do
+    expect(client.services).to eq(
+      'query' => {
+        ports: {
+          'querySoap' => {
+            type: 'http://schemas.xmlsoap.org/wsdl/soap/',
+            location: 'https://test-api.geotrust.com:443/webtrust/query.jws'
+          }
+        }
+      }
+    )
+  end
 
-    operation = client.operation('GetQuickApproverList')
-    expect(operation.name).to eq('THIS-TEST-FAILS')
+  it 'knows the operations' do
+    operations = client.operations('query', 'querySoap')
+    expect(operations).to match_array(['GetQuickApproverList', 'hello'])
+  end
+
+  it 'creates an operation with the correct endpoint' do
+    operation = client.operation('query', 'querySoap', 'GetQuickApproverList')
     expect(operation.endpoint).to eq('https://test-api.geotrust.com:443/webtrust/query.jws')
+  end
+
+  it 'creates an example body' do
+    pending "this fixture is missing a message element (s1:GetQuickApproverList)"
+
+    operation = client.operation('query', 'querySoap', 'GetQuickApproverList')
+    expect(operation.example_body).to be_a(Hash)
   end
 
 end
