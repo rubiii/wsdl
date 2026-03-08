b37efbc472d27882fff5ec2116e6be7ce24604c9
rubiii <me@rubiii.com>
2026-03-08 16:30:00 +0100

    Add specs for WSDL::HTTPAdapter::Config

---
+++ spec/wsdl/http_adapter/config_spec.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+RSpec.describe WSDL::HTTPAdapter::Config do
+  subject(:config) { described_class.new }
+
+  describe 'secure defaults' do
+    it 'uses sensible timeout defaults' do
+      expect(config.open_timeout).to eq(WSDL::HTTPAdapter::DEFAULT_OPEN_TIMEOUT)
+      expect(config.write_timeout).to eq(WSDL::HTTPAdapter::DEFAULT_WRITE_TIMEOUT)
+      expect(config.read_timeout).to eq(WSDL::HTTPAdapter::DEFAULT_READ_TIMEOUT)
+    end
+
+    it 'limits redirects by default' do
+      expect(config.max_redirects).to eq(WSDL::HTTPAdapter::DEFAULT_REDIRECT_LIMIT)
+    end
+
+    it 'enforces peer verification by default' do
+      expect(config.verify_mode).to eq(OpenSSL::SSL::VERIFY_PEER)
+    end
+
+    it 'does not set optional SSL options by default' do
+      expect(config.ca_file).to be_nil
+      expect(config.ca_path).to be_nil
+      expect(config.cert).to be_nil
+      expect(config.key).to be_nil
+      expect(config.min_version).to be_nil
+      expect(config.max_version).to be_nil
+    end
+  end
+end
