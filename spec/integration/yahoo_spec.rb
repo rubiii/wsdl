11f911ccb0697d86f266d52d6f84b12722a2c40c
rubiii <me@rubiii.com>
2026-03-21 20:13:38 +0100

    Add Yahoo integration test
    Large multi-operation service with SOAP headers
    (Security/UsernameToken), nested addresses,
    arrays of strings, and long/boolean type coercion.

--- spec/integration/yahoo_spec.rb
+++ spec/integration/yahoo_spec.rb
@@ -122,4 +122,53 @@ RSpec.describe 'Integration with Yahoo\'s AccountService' do
     expect(Nokogiri.XML(operation.to_xml))
       .to be_equivalent_to(expected).respecting_element_order
   end
+
+  context 'with a live mock service', :test_service do
+    subject(:client) { WSDL::Client.new(service.wsdl_url, strict_schema: false) }
+
+    let(:service) { WSDL::TestService[:yahoo] }
+
+    before do
+      service.start
+    end
+
+    it 'returns an advertiser account with nested types' do
+      operation = client.operation(service_name, port_name, :addManagedAdvertiser)
+
+      operation.prepare do
+        header do
+          tag('Security') do
+            tag('UsernameToken') do
+              tag('Username', 'apiuser')
+              tag('Password', 'apipass')
+            end
+          end
+          tag('license', 'LIC-001')
+          tag('accountID', 'ACCT-001')
+        end
+        body do
+          tag('addManagedAdvertiser') do
+            tag('account') do
+              tag('companyName', 'Acme Corp')
+              tag('defaultCurrency', 'USD')
+              tag('managedAccount', true)
+            end
+          end
+        end
+      end
+      response = operation.invoke
+      result = response.body[:addManagedAdvertiserResponse][:out]
+
+      expect(result[:operationSucceeded]).to be true
+
+      account = result[:account]
+      expect(account[:ID]).to eq('ADV-12345')
+      expect(account[:companyID]).to eq(67_890)
+      expect(account[:companyName]).to eq('Acme Corp')
+      expect(account[:managedAccount]).to be true
+      expect(account[:yahooOwnedAndOperatedFlag]).to be false
+      expect(account[:accountTypes][:AccountType]).to eq(['Advertiser'])
+      expect(account[:address][:city]).to eq('Sunnyvale')
+    end
+  end
 end
