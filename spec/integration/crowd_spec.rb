b4f2be62c56c87d2980d6e92c3bd12a5a490aec5
rubiii <me@rubiii.com>
2026-03-21 18:33:43 +0100

    Add Crowd integration test
    Tests deep nesting, long/boolean/dateTime type coercion, nested
    arrays (SOAPAttribute with values), and sparse responses with
    omitted optional elements.

--- spec/integration/crowd_spec.rb
+++ spec/integration/crowd_spec.rb
@@ -58,4 +58,72 @@ RSpec.describe 'Integration with Atlassian Crowd' do
 ]
     ])
   end
+
+  context 'with a live mock service', :test_service do
+    subject(:client) { WSDL::Client.new(service.wsdl_url) }
+
+    let(:service) { WSDL::TestService[:crowd] }
+    let(:service_name) { :SecurityServer }
+    let(:port_name) { :SecurityServerHttpPort }
+
+    before do
+      service.start
+    end
+
+    it 'returns a group with nested arrays and mixed types' do
+      operation = client.operation(service_name, port_name, :findGroupByName)
+
+      operation.prepare do
+        body do
+          tag('findGroupByName') do
+            tag('in0') do
+              tag('name', 'app')
+              tag('token', 'abc-123')
+            end
+            tag('in1', 'developers')
+          end
+        end
+      end
+      response = operation.invoke
+
+      group = response.body[:findGroupByNameResponse][:out]
+      expect(group[:ID]).to eq(1001)
+      expect(group[:active]).to be true
+      expect(group[:name]).to eq('developers')
+      expect(group[:directoryId]).to eq(42)
+      expect(group[:description]).to eq('Software developers')
+
+      attrs = group[:attributes][:SOAPAttribute]
+      expect(attrs).to be_an(Array)
+      expect(attrs.size).to eq(2)
+      expect(attrs[0][:name]).to eq('description')
+      expect(attrs[0][:values][:string]).to eq(['Engineering team'])
+      expect(attrs[1][:values][:string]).to eq(%w[internal ldap])
+
+      expect(group[:members][:string]).to eq(%w[alice bob charlie])
+    end
+
+    it 'handles a minimal response without optional elements' do
+      operation = client.operation(service_name, port_name, :findGroupByName)
+
+      operation.prepare do
+        body do
+          tag('findGroupByName') do
+            tag('in0') do
+              tag('name', 'app')
+              tag('token', 'abc-123')
+            end
+            tag('in1', 'nonexistent')
+          end
+        end
+      end
+      response = operation.invoke
+
+      group = response.body[:findGroupByNameResponse][:out]
+      expect(group[:ID]).to eq(0)
+      expect(group[:active]).to be false
+      expect(group[:attributes]).to be_nil
+      expect(group[:members]).to be_nil
+    end
+  end
 end
