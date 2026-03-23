46923cd41892e48122c4569293b679faaa0f3481
rubiii <me@rubiii.com>
2026-03-23 23:44:42 +0100

    Add Definition::OperationProxy, MessageProxy, and BindingProxy
    Duck-type wrappers that implement the same interface as
    Parser::OperationInfo, Parser::Input/Output, and
    Parser::BindingOperation. This allows Operation and Contract
    to work with future Definition data without modification.

--- lib/wsdl/definition.rb
+++ lib/wsdl/definition.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require 'wsdl/definition/element_hash'
+require 'wsdl/definition/operation_proxy'
 
 module WSDL
   # Abstract representation of a parsed WSDL service.
